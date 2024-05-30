5d75c71642f11ca4e2db1a6adf1d3a2d71ca54d1
naxsyambis <naxsyambis@gmail.com>
2024-05-30 11:10:54 +0100

    membuat void display dan cek apakah antrian kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -59,6 +59,15 @@ public:
                 FRONT = FRONT + 1;
         }
     }
+
+    void display() {
+
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
+    }
     
 };  
 
