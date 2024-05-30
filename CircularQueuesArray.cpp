0f373a351355e90a2eee2184a5e1308def366d80
naxsyambis <naxsyambis@gmail.com>
2024-05-30 11:52:41 +0100

    menambah front++ di bagian display

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -77,6 +77,7 @@ public:
         if (FRONT_Position <= REAR_Position) {
             while (FRONT_Position <= REAR_Position) { 
                 cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
             }
             cout << endl;
         }
